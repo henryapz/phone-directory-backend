88ce7bc723ece35cf527e3167f87ab97ce17dfc4
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2022-02-04 00:50:20 -0500

    Adding morgan package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
@@ -37,13 +38,33 @@ app
       return res.status(400).send("Name is required");
     if (!number || number.length < 3)
       return res.status(400).send("Number is required");
+
     const indexOfName = personsData.findIndex((elem) => elem.name === name);
     if (indexOfName === -1) {
-      personsData.push({ id: Math.round(Math.random()*10000), name, number });
-      res.status(201).json({ name, number });
-    }else{
-      return res.status(400).send("Name must be unique");
+      personsData.push({ id: Math.round(Math.random() * 10000), name, number });
+      return res.status(201).json({ name, number });
     }
+
+    return res.status(400).send("Name must be unique");
+  });
+
+app
+  .route("/api/persons/:idPerson")
+  .get((req, res) => {
+    const idPerson = +req.params.idPerson;
+    const person = personsData.find((elem) => elem.id === idPerson);
+
+    if (person) return res.json(person);
+    else return res.status(404).send("No record was found");
+  })
+  .delete((req, res) => {
+    const idPerson = +req.params.idPerson;
+    const personIndex = personsData.findIndex((elem) => elem.id === idPerson);
+
+    if (personIndex === -1) return res.status(404).send("No record was found");
+    
+    personsData.splice(personIndex, 1);
+    return res.json(personsData);
   });
 
 app.get("/info", (req, res) => {
@@ -52,23 +73,6 @@ app.get("/info", (req, res) => {
     `Phonebook has info for ${personsData.length} people <br>` + actualData;
   res.send(textInfo);
 });
-
-app
-  .route("/api/persons/:idPerson")
-  .get((req, res) => {
-    const idPerson = +req.params.idPerson;
-    const person = personsData.find((elem) => elem.id === idPerson);
-    if (person) return res.json(person);
-    else return res.status(404).send("No record was found");
-  })
-  .delete((req, res) => {
-    const idPerson = +req.params.idPerson;
-    const personIndex = personsData.findIndex((elem) => elem.id === idPerson);
-    if (personIndex === -1)
-      return res.status(404).send("No record was found");
-    personsData.splice(personIndex, 1);
-    return res.json(personsData);
-  });
 
 const PORT = 3001;
 app.listen(PORT, () => {
